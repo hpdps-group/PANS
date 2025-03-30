08bf30dd4db22e440580947795bd706f7f52c78f
Jinwu Yang <131998405+jinwuyang@users.noreply.github.com>
2025-03-30 08:36:21 +0800

    PANS-HIP

---
+++ examples/compress.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
+#include "ans/GpuANSEncode.h"
+#include "ans/GpuANSCodec.h"
+
+using namespace pans_hip;
+
+void compressFileWithANS(
+		const std::string& inputFilePath,//输入数据文件路径
+		const std::string& tempFilePath,//压缩后文件保存路径
+        uint32_t& batchSize,//原本数据规模
+		uint32_t& compressedSize,//压缩后数据大小
+		int precision,//ANS的精度
+		hipStream_t stream
+		) {
+    //读取输入文件
+    std::ifstream inputFile(inputFilePath, std::ios::binary | std::ios::ate);
+    std::streamsize fileSize = inputFile.tellg();
+    std::vector<uint8_t> fileData(fileSize);
+    inputFile.seekg(0, std::ios::beg);
+    inputFile.read(reinterpret_cast<char*>(fileData.data()), fileSize);//全部按照uint8_t读入
+    inputFile.close();
+
+    //传输输入文件的数据
+    uint8_t* inPtrs;
+    hipMalloc(&inPtrs, sizeof(uint8_t)*(fileSize));
+    hipMemcpy(inPtrs, fileData.data(), fileSize*sizeof(uint8_t), hipMemcpyHostToDevice);
+
+    //设置batchSize，只有一个batch
+    batchSize = fileSize;
+
+    //分配存储压缩后数据大小的GPU空间
+    uint32_t* outCompressedSize;
+    hipMalloc(&outCompressedSize, sizeof(uint32_t));
+
+    //分配存储压缩后数据的GPU空间
+    uint8_t* encPtrs;
+    hipMalloc(&encPtrs, static_cast<uint64_t>(getMaxCompressedSize(fileSize)));
+
+    std::cout<<"encode start!"<<std::endl;
+    //计时
+    auto start = std::chrono::high_resolution_clock::now();  
+
+    //压缩开始 
+    ansEncode(
+        precision,
+        inPtrs,//已经在dev
+        batchSize,
+        encPtrs,//GPU
+        outCompressedSize,//GPU
+        stream);
+    hipStreamSynchronize(stream);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    double comp_time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1e3;  
+    //计算速度
+    double c_bw = ( 1.0 * fileSize / 1e9 ) / ( comp_time * 1e-3 );  
+    //输出结果
+    std::cout << "comp   time " << std::fixed << std::setprecision(3) << comp_time << " ms B/W "   
+                  << std::fixed << std::setprecision(1) << c_bw << " GB/s " << std::endl;
+    
+    //获取压缩后的数据大小
+    uint32_t outsize;
+    hipMemcpy(&outsize, outCompressedSize, sizeof(uint32_t), hipMemcpyDeviceToHost);
+    compressedSize = outsize;
+    //printf("compressed size %d\n", compressedSize);
+
+    //保存压缩后的数据到tempFilePath
+    std::ofstream outputFile(tempFilePath, std::ios::binary);
+    std::vector<uint8_t> compressedData(outsize);
+    hipMemcpy(compressedData.data(), encPtrs, outsize*sizeof(uint8_t), hipMemcpyDeviceToHost);
+    outputFile.write(reinterpret_cast<const char*>(compressedData.data()), outsize*sizeof(uint8_t));
+    outputFile.close();
+}
+
+// void decompressFileWithANS(
+// 		const std::string& tempFilePath, //压缩文件路径
+// 		const std::string& outputFilePath,   //解压缩后文件路径
+//         uint32_t& batchSize,      //解压缩后的数据大小，原本数据大小      
+//         uint32_t& compressedSize, //压缩后的数据大小          
+// 		int precision,//精度
+// 		hipStream_t stream) {
+//     //读取压缩文件
+//     std::ifstream inFile(tempFilePath, std::ios::binary);
+//     std::vector<uint8_t> fileCompressedData(compressedSize);
+//     inFile.read(reinterpret_cast<char*>(fileCompressedData.data()), compressedSize);
+//     inFile.close();
+
+//     uint8_t* filePtrs;//传输输入数据
+//     hipMalloc(&filePtrs, sizeof(uint8_t)*(compressedSize));
+//     hipMemcpy(filePtrs,fileCompressedData.data(),compressedSize*sizeof(uint8_t),hipMemcpyHostToDevice);
+
+//     uint8_t* decPtrs;//分配保存解压缩数据的空间
+//     hipMalloc(&decPtrs, sizeof(uint8_t)*(batchSize));
+    
+//     std::cout<<"decode start!"<<std::endl;
+//     //计时
+//     double decomp_time = 0.0;
+//     auto start = std::chrono::high_resolution_clock::now();
+
+//     //解压开始
+//     ansDecode(
+//         precision,//解压缩精度
+//         filePtrs, //解压缩输入数据
+//         decPtrs,//解压缩输出数据
+//         stream);
+//     hipStreamSynchronize(stream);
+//     //printf("1\n");
+//     auto end = std::chrono::high_resolution_clock::now();  
+//     decomp_time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1e3; 
+    
+//     //计算速度
+//     double dc_bw = ( 1.0 * compressedSize / 1e9 ) / ( decomp_time * 1e-3 );
+//     //输出结果
+//     std::cout << "decomp time " << std::fixed << std::setprecision(3) << decomp_time << " ms B/W "   
+//                   << std::fixed << std::setprecision(1) << dc_bw << " GB/s" << std::endl;
+//     //保存解压后的文件到outputFilePath
+//     std::ofstream outFile(outputFilePath, std::ios::binary);
+//     std::vector<uint8_t> unCompressData(batchSize);
+//     hipMemcpy(unCompressData.data(),decPtrs,batchSize*sizeof(uint8_t),hipMemcpyDeviceToHost);
+//     outFile.write(reinterpret_cast<const char*>(unCompressData.data()), batchSize*sizeof(uint8_t));
+//     outFile.close();
+// }
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <inputfile> <tempfile> " << std::endl;
+        return 1;
+    }
+    hipStream_t stream;   
+    hipStreamCreate(&stream);
+    uint32_t batchSize;
+    uint32_t compressedSize;
+    int precision = 10; 
+    compressFileWithANS(
+        argv[1], argv[2],
+        batchSize,//压缩前数据的大小
+        compressedSize,//压缩后的数据大小
+        precision,//ANS的精度
+        stream);
+    printf("compress ratio: %f\n", 1.0 * batchSize / compressedSize);
+	// decompressFileWithANS(
+    //     argv[3],argv[4],
+    //     batchSize,//原本的数据规模s
+    //     compressedSize,//压缩后数据规模
+    //     precision,//精度
+    //     stream);
+    std::cout << "Compression completed successfully." << std::endl;
+    return 0;
+}
---
+++ include/utils/DeviceUtils.h
@@ -0,0 +1,428 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#pragma once
+#include <iostream>
+#include <hip/hip_runtime.h>
+#include <string>
+#include <vector>
+
+// #include "DeviceUtils0.h"
+#include <hip/hip_runtime_api.h>
+#include <mutex>
+#include <unordered_map>
+
+/// Wrapper to test return status of CUDA functions
+#define CUDA_VERIFY(X)                                         \
+  do {                                                         \
+    auto err__ = (X);                                          \
+    if (err__ != hipSuccess) {\
+    std::cout << "CUDA error " << pans_hip::errorToName(err__) << " "\
+              << pans_hip::errorToString(err__) << std::endl;\
+    }\
+  } while (0)
+
+/// Wrapper to synchronously probe for CUDA errors
+// #define GPU_SYNC_ERROR 1
+
+#ifdef GPU_SYNC_ERROR
+#define CUDA_TEST_ERROR()                 \
+  do {                                    \
+    CUDA_VERIFY(hipDeviceSynchronize()); \
+  } while (0)
+#else
+#define CUDA_TEST_ERROR()            \
+  do {                               \
+    CUDA_VERIFY(hipGetLastError()); \
+  } while (0)
+#endif
+
+namespace pans_hip{
+  
+constexpr int kWarpSize = 64;
+
+/// std::string wrapper around hipGetErrorString
+std::string errorToString(hipError_t err);
+
+/// std::string wrapper around hipGetErrorName
+std::string errorToName(hipError_t err);
+
+/// Returns the current thread-local GPU device
+int getCurrentDevice();
+
+/// Sets the current thread-local GPU device
+void setCurrentDevice(int device);
+
+/// Returns the number of available GPU devices
+int getNumDevices();
+
+/// Starts the CUDA profiler (exposed via SWIG)
+void profilerStart();
+
+/// Stops the CUDA profiler (exposed via SWIG)
+void profilerStop();
+
+/// Synchronizes the CPU against all devices (equivalent to
+/// hipDeviceSynchronize for each device)
+void synchronizeAllDevices();
+
+/// Returns a cached hipDeviceProp_t for the given device
+const hipDeviceProp_t& getDeviceProperties(int device);
+
+/// Returns the cached hipDeviceProp_t for the current device
+const hipDeviceProp_t& getCurrentDeviceProperties();
+
+/// Returns the maximum number of threads available for the given GPU
+/// device
+int getMaxThreads(int device);
+
+/// Equivalent to getMaxThreads(getCurrentDevice())
+int getMaxThreadsCurrentDevice();
+
+/// Returns the maximum smem available for the given GPU device
+size_t getMaxSharedMemPerBlock(int device);
+
+/// Equivalent to getMaxSharedMemPerBlock(getCurrentDevice())
+size_t getMaxSharedMemPerBlockCurrentDevice();
+
+/// For a given pointer, returns whether or not it is located on
+/// a device (deviceId >= 0) or the host (-1).
+int getDeviceForAddress(const void* p);
+
+/// Does the given device support full unified memory sharing host
+/// memory?
+bool getFullUnifiedMemSupport(int device);
+
+/// Equivalent to getFullUnifiedMemSupport(getCurrentDevice())
+bool getFullUnifiedMemSupportCurrentDevice();
+
+/// RAII object to set the current device, and restore the previous
+/// device upon destruction
+class DeviceScope {
+ public:
+  explicit DeviceScope(int device);
+  ~DeviceScope();
+
+ private:
+  int prevDevice_;
+};
+
+// RAII object to manage a hipEvent_t
+class CudaEvent {
+ public:
+  /// Creates an event and records it in this stream
+  explicit CudaEvent(hipStream_t stream, bool timer = false);
+  CudaEvent(const CudaEvent& event) = delete;
+  CudaEvent(CudaEvent&& event) noexcept;
+  ~CudaEvent();
+
+  CudaEvent& operator=(CudaEvent&& event) noexcept;
+  CudaEvent& operator=(CudaEvent& event) = delete;
+
+  inline hipEvent_t get() {
+    return event_;
+  }
+
+  /// Wait on this event in this stream
+  void streamWaitOnEvent(hipStream_t stream);
+
+  /// Have the CPU wait for the completion of this event
+  void cpuWaitOnEvent();
+
+  /// Returns the elapsed time from the other event
+  float timeFrom(CudaEvent& from);
+
+ private:
+  hipEvent_t event_;
+};
+
+// RAII object to manage a hipStream_t
+class CudaStream {
+ public:
+  /// Creates a stream on the current device
+  CudaStream(int flags = hipStreamDefault);
+  CudaStream(const CudaStream& stream) = delete;
+  CudaStream(CudaStream&& stream) noexcept;
+  ~CudaStream();
+
+  CudaStream& operator=(CudaStream&& stream) noexcept;
+  CudaStream& operator=(CudaStream& stream) = delete;
+
+  inline hipStream_t get() {
+    return stream_;
+  }
+
+  operator hipStream_t() {
+    return stream_;
+  }
+
+  static CudaStream make();
+  static CudaStream makeNonBlocking();
+
+ private:
+  hipStream_t stream_;
+};
+
+/// Call for a collection of streams to wait on
+template <typename L1, typename L2>
+void streamWaitBase(const L1& listWaiting, const L2& listWaitOn) {
+  // For all the streams we are waiting on, create an event
+  std::vector<hipEvent_t> events;
+  for (auto& stream : listWaitOn) {
+    hipEvent_t event;
+    CUDA_VERIFY(hipEventCreateWithFlags(&event, hipEventDisableTiming));
+    CUDA_VERIFY(hipEventRecord(event, stream));
+    events.push_back(event);
+  }
+
+  // For all the streams that are waiting, issue a wait
+  for (auto& stream : listWaiting) {
+    for (auto& event : events) {
+      CUDA_VERIFY(hipStreamWaitEvent(stream, event, 0));
+    }
+  }
+
+  for (auto& event : events) {
+    CUDA_VERIFY(hipEventDestroy(event));
+  }
+}
+
+/// These versions allow usage of initializer_list as arguments, since
+/// otherwise {...} doesn't have a type
+template <typename L1>
+void streamWait(const L1& a, const std::initializer_list<hipStream_t>& b) {
+  streamWaitBase(a, b);
+}
+
+template <typename L2>
+void streamWait(const std::initializer_list<hipStream_t>& a, const L2& b) {
+  streamWaitBase(a, b);
+}
+
+inline void streamWait(
+    const std::initializer_list<hipStream_t>& a,
+    const std::initializer_list<hipStream_t>& b) {
+  streamWaitBase(a, b);
+}
+
+std::string errorToString(hipError_t err) {
+  return std::string(hipGetErrorString(err));
+}
+
+std::string errorToName(hipError_t err) {
+  return std::string(hipGetErrorName(err));
+}
+
+int getCurrentDevice() {
+  int dev = -1;
+  CUDA_VERIFY(hipGetDevice(&dev));
+
+  return dev;
+}
+
+void setCurrentDevice(int device) {
+  CUDA_VERIFY(hipSetDevice(device));
+}
+
+int getNumDevices() {
+  int numDev = -1;
+  hipError_t err = hipGetDeviceCount(&numDev);
+  if (hipErrorNoDevice == err) {
+    numDev = 0;
+  } else {
+    CUDA_VERIFY(err);
+  }
+
+  return numDev;
+}
+
+void profilerStart() {
+  // CUDA_VERIFY(hipProfilerStart());
+}
+
+void profilerStop() {
+  // CUDA_VERIFY(hipProfilerStop());
+}
+
+void synchronizeAllDevices() {
+  for (int i = 0; i < getNumDevices(); ++i) {
+    DeviceScope scope(i);
+
+    CUDA_VERIFY(hipDeviceSynchronize());
+  }
+}
+
+const hipDeviceProp_t& getDeviceProperties(int device) {
+  static std::mutex mutex;
+  static std::unordered_map<int, hipDeviceProp_t> properties;
+
+  std::lock_guard<std::mutex> guard(mutex);
+
+  auto it = properties.find(device);
+  if (it == properties.end()) {
+    hipDeviceProp_t prop;
+    CUDA_VERIFY(hipGetDeviceProperties(&prop, device));
+
+    properties[device] = prop;
+    it = properties.find(device);
+  }
+
+  return it->second;
+}
+
+const hipDeviceProp_t& getCurrentDeviceProperties() {
+  return getDeviceProperties(getCurrentDevice());
+}
+
+int getMaxThreads(int device) {
+  return getDeviceProperties(device).maxThreadsPerBlock;
+}
+
+int getMaxThreadsCurrentDevice() {
+  return getMaxThreads(getCurrentDevice());
+}
+
+size_t getMaxSharedMemPerBlock(int device) {
+  return getDeviceProperties(device).sharedMemPerBlock;
+}
+
+size_t getMaxSharedMemPerBlockCurrentDevice() {
+  return getMaxSharedMemPerBlock(getCurrentDevice());
+}
+
+int getDeviceForAddress(const void* p) {
+  if (!p) {
+    return -1;
+  }
+
+  hipPointerAttribute_t att;
+  hipError_t err = hipPointerGetAttributes(&att, p);
+
+  if (err == hipErrorInvalidValue) {
+    // Make sure the current thread error status has been reset
+    err = hipGetLastError();
+
+    return -1;
+  }
+
+  // memoryType is deprecated for CUDA 10.0+
+#if CUDA_VERSION < 10000
+  if (att.memoryType == hipMemoryTypeHost) {
+    return -1;
+  } else {
+    return att.device;
+  }
+#else
+  // FIXME: what to use for managed memory?
+  if (att.type == hipMemoryTypeDevice) {
+    return att.device;
+  } else {
+    return -1;
+  }
+#endif
+}
+
+bool getFullUnifiedMemSupport(int device) {
+  const auto& prop = getDeviceProperties(device);
+  return (prop.major >= 6);
+}
+
+bool getFullUnifiedMemSupportCurrentDevice() {
+  return getFullUnifiedMemSupport(getCurrentDevice());
+}
+
+DeviceScope::DeviceScope(int device) {
+  if (device >= 0) {
+    int curDevice = getCurrentDevice();
+
+    if (curDevice != device) {
+      prevDevice_ = curDevice;
+      setCurrentDevice(device);
+      return;
+    }
+  }
+
+  // Otherwise, we keep the current device
+  prevDevice_ = -1;
+}
+
+DeviceScope::~DeviceScope() {
+  if (prevDevice_ != -1) {
+    setCurrentDevice(prevDevice_);
+  }
+}
+
+// CudaEvent::CudaEvent(hipStream_t stream, bool timer) : event_(nullptr) {
+//   CUDA_VERIFY(hipEventCreateWithFlags(
+//       &event_, timer ? hipEventDefault : hipEventDisableTiming));
+//   CUDA_VERIFY(hipEventRecord(event_, stream));
+// }
+
+// CudaEvent::CudaEvent(CudaEvent&& event) noexcept
+//     : event_(std::move(event.event_)) {
+//   event.event_ = nullptr;
+// }
+
+// CudaEvent::~CudaEvent() {
+//   if (event_) {
+//     CUDA_VERIFY(hipEventDestroy(event_));
+//   }
+// }
+
+// CudaEvent& CudaEvent::operator=(CudaEvent&& event) noexcept {
+//   event_ = std::move(event.event_);
+//   event.event_ = nullptr;
+
+//   return *this;
+// }
+
+// void CudaEvent::streamWaitOnEvent(hipStream_t stream) {
+//   CUDA_VERIFY(hipStreamWaitEvent(stream, event_, 0));
+// }
+
+// void CudaEvent::cpuWaitOnEvent() {
+//   CUDA_VERIFY(hipEventSynchronize(event_));
+// }
+
+// float CudaEvent::timeFrom(CudaEvent& from) {
+//   cpuWaitOnEvent();
+//   float ms = 0;
+//   CUDA_VERIFY(hipEventElapsedTime(&ms, from.event_, event_));
+
+//   return ms;
+// }
+
+// CudaStream::CudaStream(int flags) : stream_(nullptr) {
+//   CUDA_VERIFY(hipStreamCreateWithFlags(&stream_, flags));
+// }
+
+// CudaStream::CudaStream(CudaStream&& stream) noexcept
+//     : stream_(std::move(stream.stream_)) {
+//   stream.stream_ = nullptr;
+// }
+
+// CudaStream::~CudaStream() {
+//   if (stream_) {
+//     CUDA_VERIFY(hipStreamDestroy(stream_));
+//   }
+// }
+
+// CudaStream& CudaStream::operator=(CudaStream&& stream) noexcept {
+//   stream_ = std::move(stream.stream_);
+//   stream.stream_ = nullptr;
+
+//   return *this;
+// }
+
+// CudaStream CudaStream::make() {
+//   return CudaStream();
+// }
+
+// CudaStream CudaStream::makeNonBlocking() {
+//   return CudaStream(hipStreamNonBlocking);
+// }
+
+}
